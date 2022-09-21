3318780153c072d2b43d6e5ca76ad4065008b440
vojeda <vojeda@set.gov.py>
2022-09-20 23:36:56 -0400

    Updates

---
+++ precache-manifest.fec678ed0a2a8e2726f1e3e543338dd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7aa3376b7158208e18e53f5889b4221",
+    "url": "/poke-api-react/index.html"
+  },
+  {
+    "revision": "b8b745e7f77c39d729fa",
+    "url": "/poke-api-react/static/css/main.cd61a7db.chunk.css"
+  },
+  {
+    "revision": "d26f4d10ba9d5771b0ed",
+    "url": "/poke-api-react/static/js/2.7dd13ad8.chunk.js"
+  },
+  {
+    "revision": "b8b745e7f77c39d729fa",
+    "url": "/poke-api-react/static/js/main.3c17b1f2.chunk.js"
+  },
+  {
+    "revision": "0ee2326afb02536c6ac5",
+    "url": "/poke-api-react/static/js/runtime~main.e41dd621.js"
+  }
+]);
